2200ab34f7f71c344f750b8089a171f763faf487
cq_bomb <collinsctk@qytang.com>
2020-03-23 15:30:56 +0800

    网络编程第三期

--- net_5_tftp/tftp_client.py
+++ net_5_tftp/tftp_client.py
@@ -7,17 +7,22 @@
 # https://ke.qq.com/course/271956?tuin=24199d8a
 
 
-from part1_classic_protocols.tools.minimumTFTP import Server
+from tools.minimumTFTP import Client
 
 
-def qyt_tftpserver(tftp_dir):
-    print("TFTP服务器准备就绪,根目录为", tftp_dir)
-    tftpServer = Server(tftp_dir)
-    tftpServer.run()
+def qyt_ftpclient(server, filedir, file, operation=1):
+    tftp_client = Client(server, filedir, file)
+    if operation == 1:
+        tftp_client.get()
+    if operation == 2:
+        tftp_client.put()
+    print()
 
 
 if __name__ == '__main__':
     # 使用Linux解释器 & WIN解释器
     # 正常安装有问题,需要把minimumTFTP.py文件放入如下的路径
     # /usr/local/lib/python3.6/site-packages/tools/minimumTFTP.py
-    qyt_tftpserver('./tftpdir')
+
+    # qyt_ftpclient('10.1.1.100', '.', 'testupload.txt', operation=1)
+    qyt_ftpclient('10.1.1.100', '.', 'testupload.txt', operation=2)
--- net_5_tftp/tftp_server.py
+++ net_5_tftp/tftp_server.py
@@ -7,22 +7,17 @@
 # https://ke.qq.com/course/271956?tuin=24199d8a
 
 
-from part1_classic_protocols.tools.minimumTFTP import Client
+from tools.minimumTFTP import Server
 
 
-def qyt_ftpclient(server, filedir, file, operation=1):
-    tftpClient = Client(server, filedir, file)
-    if operation == 1:
-        tftpClient.get()
-    if operation == 2:
-        tftpClient.put()
-    print()
+def qyt_tftpserver(tftp_dir):
+    print("TFTP服务器准备就绪,根目录为", tftp_dir)
+    tftpServer = Server(tftp_dir)
+    tftpServer.run()
 
 
 if __name__ == '__main__':
     # 使用Linux解释器 & WIN解释器
     # 正常安装有问题,需要把minimumTFTP.py文件放入如下的路径
     # /usr/local/lib/python3.6/site-packages/tools/minimumTFTP.py
-
-    # qyt_ftpclient('10.1.1.100', '.', 'testupload.txt', operation=1)
-    qyt_ftpclient('10.1.1.100', '.', 'testupload.txt', operation=2)
+    qyt_tftpserver('./tftp_dir')
