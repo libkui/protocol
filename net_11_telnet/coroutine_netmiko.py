9bec24cfc3280f117894d60e06a779832fd85984
cq_bomb <collinsctk@qytang.com>
2020-04-04 16:48:11 +0800

    计算时间! 更多地址!

--- net_11_telnet/coroutine_netmiko.py
+++ net_11_telnet/coroutine_netmiko.py
@@ -3,8 +3,8 @@ from netmiko import Netmiko
 CSR = {
         'username': 'admin',
         'password': 'Cisc0123',
-        'device_type': 'cisco_ios_telnet',  # Telnet
-        # 'device_type': 'cisco_ios',  # SSH
+        # 'device_type': 'cisco_ios_telnet',  # Telnet
+        'device_type': 'cisco_ios',  # SSH
         'secret': 'Cisc0123'
         }
 
@@ -21,17 +21,32 @@ def netmiko_show(device_ip, cmd):
 
 if __name__ == '__main__':
     # 下面是协程实战内容
+    from datetime import datetime
     import gevent
     from gevent import monkey
     monkey.patch_all()
     # 设备清单
-    devices_list = ['10.1.1.253', '10.1.1.252']
+    devices_list = ['10.1.1.253', '10.1.1.252', '10.1.1.251', '10.1.1.250', '10.1.1.249']
     # 把ip和cmd放到一个列表, 便于后续使用*device来传多参数
     devices_cmd_list = [[d, 'show ip interface brief'] for d in devices_list]
     # 多参数使用*device来传
+
+    # 携程部分
+    start_time = datetime.now()
     tasks = [gevent.spawn(netmiko_show, *device) for device in devices_cmd_list]
     all_result = gevent.joinall(tasks)
     for s in all_result:
         if s.get():
             print('='*40 + s.get()[0] + '='*40)
             print(s.get()[1])
+    end_time = datetime.now()
+    print((end_time - start_time).seconds)
+
+    # 普通操作
+    # start_time = datetime.now()
+    # for d in devices_cmd_list:
+    #     ip, cmd_result = netmiko_show(*d)
+    #     print('=' * 40 + ip + '=' * 40)
+    #     print(cmd_result)
+    # end_time = datetime.now()
+    # print((end_time - start_time).seconds)
