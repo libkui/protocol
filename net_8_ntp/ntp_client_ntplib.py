95951962574edffbfdfdb958fdfa75335877902b
cq_bomb <collinsctk@qytang.com>
2020-08-01 08:59:51 +0800

    注释!

--- net_8_ntp/ntp_client_ntplib.py
+++ net_8_ntp/ntp_client_ntplib.py
@@ -14,6 +14,9 @@ from time import ctime
 def ntp_client(ntp_server):
     c = ntplib.NTPClient()
     response = c.request(ntp_server, version=3)
+    # print(response.tx_time) # 1596243472.7740922
+    # ctime() 函数把一个时间戳（按秒计算的浮点数）转化为time.asctime()的形式
+    # Sat Aug  1 08:57:52 2020
     print('\t' + ctime(response.tx_time))
 
 
