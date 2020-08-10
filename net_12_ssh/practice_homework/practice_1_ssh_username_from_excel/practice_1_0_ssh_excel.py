8a362514946ab35fc7dd4c601e6c80bcd696e5b6
cq_bomb <collinsctk@qytang.com>
2020-08-10 20:50:16 +0800

    inti

--- net_12_ssh/practice_homework/practice_1_ssh_username_from_excel/practice_1_0_ssh_excel.py
+++ net_12_ssh/practice_homework/practice_1_ssh_username_from_excel/practice_1_0_ssh_excel.py
@@ -7,18 +7,22 @@
 # https://ke.qq.com/course/271956?tuin=24199d8a
 
 from net_12_ssh.practice_homework.practice_1_ssh_username_from_excel.practice_0_parser_excel_return_dict import excel_parser_return_dict
+from net_12_ssh.practice_homework.practice_1_ssh_username_from_excel.practice_0_parser_excel_return_dict import excel_parser_return_list
 from net_12_ssh.practice_homework.practice_1_ssh_username_from_excel.practice_0_write_excel_openpyxl import excel_write
+from net_12_ssh.practice_homework.practice_1_ssh_username_from_excel.practice_0_write_excel_openpyxl import excel_write_list
 from net_12_ssh.ssh_sftp.ssh_client_netmiko import netmiko_show_cred, netmiko_config_cred
 import re
 
 
 def excel_user_to_ios(ip, username, password, excelfile):
     # 读取excel信息，返回字典！
-    user_dict = excel_parser_return_dict(excelfile)
+    user_list = excel_parser_return_list(excelfile)
     # 产生配置命令
     cmds = ['configure terminal']
-    for x, y in user_dict.items():
-        cmd = 'username ' + x + ' privilege ' + str(y[1]) + ' password ' + str(y[0])
+    for user_dict in user_list:
+        cmd = f"username {user_dict.get('username')} " \
+              f"privilege {user_dict.get('privilege')} " \
+              f"password {user_dict.get('password')}"
         cmds.append(cmd)
     # ssh登录路由器，配置用户信息
     netmiko_config_cred(ip, username, password, cmds, verbose=False)
@@ -29,21 +33,27 @@ def excel_ios_user_to_excel(ip, username, password, excelfile):
     show_run = netmiko_show_cred(ip, username, password, 'sh run | in username')
     # 把结果通过'\r\n'分离，产生清单
     show_run_list = show_run.split('\n')
-    user_dict = {}
+    user_list = []
     for x in show_run_list:
         # 如果格式为username admin privilege 15 password 0 cisco，提取用户名，密码和级别
         if re.match(r'username (\w+) privilege (\d+) password \w (\w+)', x):
             re_result = re.match(r'username (\w+) privilege (\d+) password \w (\w+)', x).groups()
-            user_dict[re_result[0]] = re_result[2], int(re_result[1])
+            user_list.append({'username': re_result[0],
+                              'privilege': int(re_result[1]),
+                              'password': re_result[2],
+                              })
         # 如果格式为username passuser password 0 12345，提取用户和密码，级别为1级
         elif re.match(r'username (\w+) password \w (\w+)', x):
             re_result = re.match(r'username (\w+) password \w (\w+)', x).groups()
-            user_dict[re_result[0]] = re_result[1], 1
+            user_list.append({'username': re_result[0],
+                              'privilege': 1,
+                              'password': re_result[1],
+                              })
     # 把字典的用户名，密码和级别信息，写入Excel
     # sheel_name为IP地址
-    excel_write(file=excelfile, sheel_name=ip, write_dict=user_dict)
+    excel_write_list(file=excelfile, sheel_name=ip, write_list=user_list)
 
 
 if __name__ == '__main__':
-    excel_user_to_ios('10.1.1.253', 'admin', 'Cisc0123', './excel_file/read_accounts.xlsx')
+    # excel_user_to_ios('10.1.1.253', 'admin', 'Cisc0123', './excel_file/read_accounts.xlsx')
     excel_ios_user_to_excel('10.1.1.253', 'admin', 'Cisc0123', './excel_file/write_iosuser_new.xlsx')
