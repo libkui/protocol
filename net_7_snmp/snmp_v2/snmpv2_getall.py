2d1380f3623f803bca1a930e3f48dd75ccc8d692
cq_bomb <collinsctk@qytang.com>
2021-04-06 06:56:05 +0800

    更新为整数

--- net_7_snmp/snmp_v2/snmpv2_getall.py
+++ net_7_snmp/snmp_v2/snmpv2_getall.py
@@ -15,13 +15,13 @@ def snmpv2_getall(ip, community, count=25, port=161):
     name_list = [x[1] for x in snmpv2_getbulk(ip, community, "1.3.6.1.2.1.2.2.1.2", count=count, port=port)]
     # print(name_list)
 
-    speed_list = [x[1] for x in snmpv2_getbulk(ip, community, "1.3.6.1.2.1.2.2.1.5", count=count, port=port)]
+    speed_list = [int(x[1]) for x in snmpv2_getbulk(ip, community, "1.3.6.1.2.1.2.2.1.5", count=count, port=port)]
     # print(speed_list)
 
-    in_bytes_list = [x[1] for x in snmpv2_getbulk(ip, community, "1.3.6.1.2.1.2.2.1.10", count=count, port=port)]
+    in_bytes_list = [int(x[1]) for x in snmpv2_getbulk(ip, community, "1.3.6.1.2.1.2.2.1.10", count=count, port=port)]
     # print(in_bytes_list)
 
-    out_bytes_list = [x[1] for x in snmpv2_getbulk(ip, community, "1.3.6.1.2.1.2.2.1.16", count=count, port=port)]
+    out_bytes_list = [int(x[1]) for x in snmpv2_getbulk(ip, community, "1.3.6.1.2.1.2.2.1.16", count=count, port=port)]
     # print(out_bytes_list)
 
     if_list = []
@@ -29,9 +29,9 @@ def snmpv2_getall(ip, community, count=25, port=161):
         if_list.append({'name': x[0], 'speed': x[1], 'in_bytes': x[2], 'out_bytes': x[3]})
 
     final_dict = {'ip': ip,
-                  'cpu_usage': cpu_usage[1],
-                  'mem_usage': mem_usage[1],
-                  'mem_free': mem_free[1],
+                  'cpu_usage': int(cpu_usage[1]),
+                  'mem_usage': int(mem_usage[1]),
+                  'mem_free': int(mem_free[1]),
                   'if_list': if_list}
 
     return  final_dict
