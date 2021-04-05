170578db590a8886315503830cef819e89068fe3
cq_bomb <collinsctk@qytang.com>
2021-04-06 06:52:21 +0800

    更新getall

--- net_7_snmp/snmp_v2/snmpv2_getall.py
+++ net_7_snmp/snmp_v2/snmpv2_getall.py
@@ -1,7 +1,17 @@
 from snmpv2_getbulk import snmpv2_getbulk
+from snmpv2_get import snmpv2_get
 
 
 def snmpv2_getall(ip, community, count=25, port=161):
+    # cpmCPUTotal5sec
+    cpu_usage = snmpv2_get(ip, community, "1.3.6.1.4.1.9.9.109.1.1.1.1.3.7", port=161)
+
+    # cpmCPUMemoryUsed
+    mem_usage = snmpv2_get(ip, community, "1.3.6.1.4.1.9.9.109.1.1.1.1.12.7", port=161)
+
+    # cpmCPUMemoryFree
+    mem_free = snmpv2_get(ip, community, "1.3.6.1.4.1.9.9.109.1.1.1.1.13.7", port=161)
+
     name_list = [x[1] for x in snmpv2_getbulk(ip, community, "1.3.6.1.2.1.2.2.1.2", count=count, port=port)]
     # print(name_list)
 
@@ -14,16 +24,22 @@ def snmpv2_getall(ip, community, count=25, port=161):
     out_bytes_list = [x[1] for x in snmpv2_getbulk(ip, community, "1.3.6.1.2.1.2.2.1.16", count=count, port=port)]
     # print(out_bytes_list)
 
-    final_list = []
+    if_list = []
     for x in zip(name_list, speed_list, in_bytes_list, out_bytes_list):
-        final_list.append({'name': x[0], 'speed': x[1], 'in_bytes': x[2], 'out_bytes': x[3]})
+        if_list.append({'name': x[0], 'speed': x[1], 'in_bytes': x[2], 'out_bytes': x[3]})
 
-    return final_list
+    final_dict = {'ip': ip,
+                  'cpu_usage': cpu_usage[1],
+                  'mem_usage': mem_usage[1],
+                  'mem_free': mem_free[1],
+                  'if_list': if_list}
+
+    return  final_dict
 
 
 if __name__ == '__main__':
     from pprint import pprint
 
-    final_list_result = snmpv2_getall("10.1.1.253", "tcpipro", count=25, port=161)
+    final_result = snmpv2_getall("10.1.1.253", "tcpipro", count=25, port=161)
 
-    pprint(final_list_result, indent=4)
+    pprint(final_result, indent=4)
