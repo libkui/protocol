6014298c3fbf3dca9c0b19dd927b46ddc3750995
cq_bomb <collinsctk@qytang.com>
2021-04-06 16:46:06 +0800

    返回datetime对象

--- net_8_ntp/ntp_client_ntplib.py
+++ net_8_ntp/ntp_client_ntplib.py
@@ -9,17 +9,16 @@
 
 import ntplib
 from time import ctime
+import datetime
 
 
 def ntp_client(ntp_server):
     c = ntplib.NTPClient()
     response = c.request(ntp_server, version=3)
-    # print(response.tx_time) # 1596243472.7740922
-    # ctime() 函数把一个时间戳（按秒计算的浮点数）转化为time.asctime()的形式
-    # Sat Aug  1 08:57:52 2020
-    print('\t' + ctime(response.tx_time))
+    # print(response.tx_time) # 1596243472.7740922 为timestamp
+    return datetime.datetime.fromtimestamp(response.tx_time)
 
 
 if __name__ == '__main__':
     # 使用Linux解释器 & WIN解释器
-    ntp_client('0.cn.pool.ntp.org')
+    print(ntp_client('0.cn.pool.ntp.org'))
