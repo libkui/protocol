a5fd35b5a1fd99d5b7db6dd8d7527b233de216e6
cq_bomb <collinsctk@qytang.com>
2021-07-27 09:59:47 +0800

    添加pyats部分!

---
+++ net_12_ssh/ssh_sftp/cisco_pyats/ssh_1_netmiko_pyats_command_parse.py
@@ -0,0 +1,13 @@
+# pip3 install pyats_genie_command_parse
+from pyats_genie_command_parse import GenieCommandParse
+from net_12_ssh.ssh_sftp.ssh_client_netmiko import netmiko_show_cred
+from pprint import pprint
+
+
+raw_result = netmiko_show_cred('10.1.1.253', 'admin', 'Cisc0123', 'show ip inter brie')
+print(raw_result)
+
+parse_obj = GenieCommandParse(nos='ios')
+data = parse_obj.parse_string(show_command='show ip interface brief',
+                              show_output_data=raw_result)
+pprint(data)
