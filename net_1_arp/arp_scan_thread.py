3126d77fc77171c2e40ae2b99abdb1849ca55059
Administrator <Cisc0123>
2021-08-01 22:10:42 +0800

    多进程!linux有效!

--- net_1_arp/arp_scan_thread.py
+++ net_1_arp/arp_scan_thread.py
@@ -11,17 +11,18 @@ import logging
 
 logging.getLogger("kamene.runtime").setLevel(logging.ERROR)
 import ipaddress
-from multiprocessing.pool import ThreadPool
+from multiprocessing.pool import Pool as ProcessPool
+from multiprocessing import freeze_support
 from net_1_arp.arp_request import arp_request
 from net_1_arp.time_decorator import run_time
 
 
 @run_time()
-def scapy_arp_scan(network, ifname):
+def scapy_arp_scan(network):
     net = ipaddress.ip_network(network)  # 产生网络对象
     ip_list = [str(ip_add) for ip_add in net]  # 把网络中的IP放入ip_list
-    pool = ThreadPool(processes=100)  # 创建多进程的进程池（并发为100）
-    result = [pool.apply_async(arp_request, args=(i, ifname)) for i in ip_list]  # 把线程放入result清单
+    pool = ProcessPool(processes=100)  # 创建多进程的进程池（并发为100）
+    result = [pool.apply_async(arp_request, args=(i,)) for i in ip_list]  # 把线程放入result清单
     pool.close()  # 关闭pool，不再加入新的线程
     pool.join()  # 等待每一个线程结束
     scan_dict = {}  # ARP扫描结果的字典, 键为IP, 值为MAC
@@ -33,6 +34,7 @@ def scapy_arp_scan(network, ifname):
 
 if __name__ == '__main__':
     # Windows Linux均可使用
-    for ip, mac in scapy_arp_scan("10.1.1.0/24", 'ens33').items():
+    freeze_support()
+    for ip, mac in scapy_arp_scan("10.1.1.0/24").items():
         print('ip地址:'+ip+'是活动的,他的MAC地址是:'+mac)
 
