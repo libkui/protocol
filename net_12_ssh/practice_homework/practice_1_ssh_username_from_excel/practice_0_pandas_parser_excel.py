89350518492cc398b5c2b1d7b9de0c222dce36d4
Administrator <Cisc0123>
2021-12-14 17:43:59 +0800

    优化打印

--- net_12_ssh/practice_homework/practice_1_ssh_username_from_excel/practice_0_pandas_parser_excel.py
+++ net_12_ssh/practice_homework/practice_1_ssh_username_from_excel/practice_0_pandas_parser_excel.py
@@ -1,4 +1,3 @@
-import xlrd
 import pandas as pd
 
 
@@ -7,7 +6,9 @@ def excel_parser_return_list(file='test.xlsx', sheel_name='Sheet1'):
     # print(username_info)
     return_list = []
     for i in username_info.index.values:
+        print('='*50 + str(i) + '='*50)
         zidian = username_info.loc[i, ['用户', '密码', '级别']].to_dict()
+        print(zidian)
         return_dict = {'username': zidian.get('用户'),
                        'password': zidian.get('密码'),
                        'privilege': zidian.get('级别')}
