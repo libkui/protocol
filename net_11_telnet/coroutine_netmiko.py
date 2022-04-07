0f8dd8a33cbd4662a0d37f99b1bf0084ae36cece
Administrator <Cisc0123>
2022-04-07 17:56:23 +0800

    改为毫秒

--- net_11_telnet/coroutine_netmiko.py
+++ net_11_telnet/coroutine_netmiko.py
@@ -28,8 +28,8 @@ if __name__ == '__main__':
     # 设备清单
     devices_list = ['10.1.1.253', '10.1.1.252']
     # 把ip和cmd放到一个列表, 便于后续使用*device来传多参数
-    devices_cmd_list = [[d, 'show ip interface brief'] for d in devices_list]
-    # device_cmd = [['10.1.1.253', 'show ip interface brief'], ['10.1.1.252', 'show ip interface brief'], ]
+    devices_cmd_list = [[d, 'show run'] for d in devices_list]
+    # devices_cmd_list = [['10.1.1.253', 'show ip interface brief'], ['10.1.1.252', 'show ip interface brief'], ]
     # 多参数使用*device来传
 
     # 协程部分
@@ -41,7 +41,7 @@ if __name__ == '__main__':
             print('='*40 + s.get()[0] + '='*40)
             print(s.get()[1])
     end_time = datetime.now()
-    print((end_time - start_time).seconds)
+    print((end_time - start_time).microseconds)
 
     # 普通操作
     # start_time = datetime.now()
@@ -50,4 +50,4 @@ if __name__ == '__main__':
     #     print('=' * 40 + ip + '=' * 40)
     #     print(cmd_result)
     # end_time = datetime.now()
-    # print((end_time - start_time).seconds)
+    # print((end_time - start_time).microseconds)
