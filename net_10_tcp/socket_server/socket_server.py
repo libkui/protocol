8d9bbba4274d5a737ebf49ff9a221a90b55c5bb6
Administrator <Cisc0123>
2022-07-26 16:53:31 +0800

    mss

--- net_10_tcp/socket_server/socket_server.py
+++ net_10_tcp/socket_server/socket_server.py
@@ -22,6 +22,7 @@ sockobj.bind((myHost, myPort))
 # 在拒绝连接前，操作系统可以挂起的最大连接数量，一般配置为5
 # 注意:并不支持并发!
 sockobj.listen(5)
+mss = 1460
 print('Server Started!')
 while True:  # 一直接受请求，直到ctl+c终止程序
     # 接受TCP连接，并且返回（conn,address）的元组，conn为新的套接字对象，可以用来接收和发送数据，address是连接客户端的地址
@@ -29,7 +30,7 @@ while True:  # 一直接受请求，直到ctl+c终止程序
     # 打印连接客户端的IP地址
     print('Server Connected by', address)
     while True:
-        data = connection.recv(1024)  # 接收数据，1024为bufsize，表示一次接收的最大数据量！
+        data = connection.recv(mss)  # 接收数据，1024为bufsize，表示一次接收的最大数据量！
         if not data:
             break  # 如果没有数据就退出循环
         # 添加时间并发送回显数据给客户，注意Python3.x后，发送和接收的数据必须为二进制！
