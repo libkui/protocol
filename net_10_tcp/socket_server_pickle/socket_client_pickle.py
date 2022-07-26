05fa5006f439ab5db801e771b42aa6b24d48f93f
Administrator <Cisc0123>
2022-07-26 17:14:36 +0800

    mss

--- net_10_tcp/socket_server_pickle/socket_client_pickle.py
+++ net_10_tcp/socket_server_pickle/socket_client_pickle.py
@@ -16,11 +16,14 @@ def client_pickle(ip, port, obj):
     msg = pickle.dumps(obj)  # 把obj pickle到一个二进制字串
     sockobj = socket(AF_INET, SOCK_STREAM)
     sockobj.connect((ip, port))
+
+    mss = 1460
+
     send_message = BytesIO(msg)  # 由于二进制字串无法按照长度读取，所以给他写到一个BytesIO
-    send_message_fragment = send_message.read(1024)  # 读取1024字节长度数据
+    send_message_fragment = send_message.read(mss)  # 读取mss字节长度数据
     while send_message_fragment:
         sockobj.send(send_message_fragment)  # 发送数据分片（如果分片的话）
-        send_message_fragment = send_message.read(1024)  # 继续读取数据
+        send_message_fragment = send_message.read(mss)  # 继续读取数据
     print('Pickle File Sended')
     sockobj.close()
 
@@ -30,5 +33,5 @@ if __name__ == '__main__':
     # Server和Client需要分属两个不同的机器
     send_dict = {'key1': 'welcome to qytang' * 2048, 'key2': [1, 2, 3, 4, 5], 'key3': ([3, 4], 'python')}
     myfile = open('./file_dir/Logo.jpg', 'rb').read()
-    client_pickle('10.1.1.80', 8888, send_dict)
-    client_pickle('10.1.1.80', 8888, myfile)
+    client_pickle('10.1.1.80', 5555, send_dict)
+    client_pickle('10.1.1.80', 5555, myfile)
--- net_10_tcp/socket_server_pickle/socket_server_pickle.py
+++ net_10_tcp/socket_server_pickle/socket_server_pickle.py
@@ -19,16 +19,18 @@ def server_pickle(ip, port):
     # 在拒绝连接前，操作系统可以挂起的最大连接数量，一般配置为5
     sockobj.listen(5)
 
+    mss = 1460
+
     while True:  # 一直接受请求，直到ctl+c终止程序
         # 接受TCP连接，并且返回（conn,address）的元组，conn为新的套接字对象，可以用来接收和发送数据，address是连接客户端的地址
         connection, address = sockobj.accept()
         # 打印连接客户端的IP地址
         print('Server Connected by', address)
         recieved_message = b''  # 预先定义接收信息变量
-        recieved_message_fragment = connection.recv(1024)  # 读取接收到的信息，写入到接收到信息分片
+        recieved_message_fragment = connection.recv(mss)  # 读取接收到的信息，写入到接收到信息分片
         while recieved_message_fragment:
             recieved_message = recieved_message + recieved_message_fragment  # 把所有接收到信息分片重组装
-            recieved_message_fragment = connection.recv(1024)
+            recieved_message_fragment = connection.recv(mss)
         obj = pickle.loads(recieved_message)  # 把接收到信息pickle回正常的obj
         if isinstance(obj, dict):
             print("收到字典数据!!!")
@@ -45,5 +47,5 @@ if __name__ == '__main__':
     # 使用Linux解释器 & WIN解释器
     # Server和Client需要分属两个不同的机器
     server_ip = '0.0.0.0'
-    server_port = 8888
+    server_port = 5555
     server_pickle(server_ip, server_port)
